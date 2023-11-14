fa83fbfc190031685a3716902697ca11f8a3bc7c
MiikkaJanesaro <122359891+MiikkaJanesaro@users.noreply.github.com>
2023-11-14 15:40:19 +0200

    authorization

--- postgre/person.js
+++ postgre/person.js
@@ -1,7 +1,9 @@
 const pgPool = require('./connection');
 
 const database = {
-    INSERT_PERSON: 'INSERT INTO person (username, password, email, createdate) VALUES ($1, $2, $3, $4)'
+    INSERT_PERSON: 'INSERT INTO person (username, password, email, createdate) VALUES ($1, $2, $3, $4)',
+    GET_PERSON: 'SELECT username,password,email,createdate FROM customer',
+    GET_PW: 'SELECT pw FROM customer WHERE username=$1'
 };
 
 addPerson('matti', 'matti1234', 'mattimei3@mattila.fi', new Date());
@@ -9,3 +11,22 @@ addPerson('matti', 'matti1234', 'mattimei3@mattila.fi', new Date());
 async function addPerson(username, password, email, createdate){
     await pgPool.query(database.INSERT_PERSON, [username, password, email, createdate])
 }
+
+async function getPerson(){
+    const result = await psPool.query(database.GET_PERSON);
+    const rows = result.rows;
+    return rows;
+}
+
+async function checkPerson(username){
+    const result = await pgPool.query(database.GET_PW, [username]);
+
+    if(result.rows.length > 0){
+        return result.rows[0].pw;
+    }else{
+        return null;
+    }
+
+}
+
+module.exports = {addPerson, getPerson, checkPerson};
