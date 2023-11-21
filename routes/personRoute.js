a4b4f941e657a6180c4331d6abb10aed2611dba5
Temkhu <teemu.ritatorma@gmail.com>
2023-11-21 21:08:38 +0200

    a
    a

--- routes/personRoute.js
+++ routes/personRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 const upload = multer({dest: 'upload/'});
-const {addPerson, deletePerson, getPerson, updatePassword, updateEmail} = require('../postgre/person');
+const {addPerson, deletePerson, getPerson, updatePassword, updateEmail, checkEmail, checkUsername} = require('../postgre/person');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 
@@ -9,48 +9,86 @@ router.post('/register', upload.none(), async (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
     const email = req.body.email;
-    
-    try{
-        const hashPw = await bcrypt.hash(password, 10);
-        const createdate = new Date();
-        await addPerson(username, hashPw, email, createdate);
-        res.json({ message: 'Käyttäjän luonti onnistui' });
-    } catch (error){
-        res.json({error: error.message}).status(505);
+    if(!username || !password || !email){
+        return res.status(400).json({ error: 'Käyttäjänimi, salasana ja email tarvitaan.' });
+    }else{
+        try{
+            if(await checkEmail(email)){
+                return res.status(400).json({ error: 'Email on jo käytössä.' });
+            }      
+            if(await checkUsername(username)){
+                return res.status(400).json({ error: 'Käyttäjätunnus on varattu.' });
+            }
+            const hashPw = await bcrypt.hash(password, 10);
+            const createdate = new Date();
+            await addPerson(username, hashPw, email, createdate);
+            res.json({ message: 'Käyttäjän luonti onnistui' });
+        } catch (error){
+            res.json({error: error.message}).status(505);
+        }
     }
 });
 
 router.post('/delete', upload.none(), async (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
-    try {
-        await deletePerson(username, password);
-        res.json({ message: 'Käyttäjän poistaminen onnistui' });
-    } catch (error){
-        res.json({error: error.message}).status(505);
+    if (!username || !password){
+        return res.status(400).json({ error: 'Käyttäjänimi ja salasana tarvitaan.' });
+    }else{
+        try {
+            await deletePerson(username, password);
+            res.json({ message: 'Käyttäjän poistaminen onnistui' });
+        } catch (error){
+            res.json({error: error.message}).status(505);
+        }
     }
 });
 
 router.post('/updatepassword', upload.none(), async (req, res) => {
     const password = req.body.password;
     const username = req.body.username;
-    try {
-        const hashPw = await bcrypt.hash(password, 10); 
-        await updatePassword(hashPw, username);
-        res.json({ message: 'Käyttäjän salasanan vaihto onnistui' });
-    } catch (error){
-        res.json({error: error.message}).status(505);
+    if (!password || !username){
+        return res.status(400).json({ error: 'Käyttäjänimi ja salasana tarvitaan.' });
+    }else{
+        try {
+            const hashPw = await bcrypt.hash(password, 10); 
+            await updatePassword(hashPw, username);
+            res.json({ message: 'Käyttäjän salasanan vaihto onnistui' });
+        } catch (error){
+            res.json({error: error.message}).status(505);
+        }
     }
 });
 
 router.post('/updateemail', upload.none(), async (req, res) => {
     const email = req.body.email;
     const username = req.body.username;
+if (!username || !email){
+    return res.status(400).json({ error: 'Käyttäjänimi ja email tarvitaan.' });
+}else{
     try {
+        if(await checkEmail(email)){
+            return res.status(400).json({ error: 'Email on jo käytössä.' });
+        }      
         await updateEmail(email, username);
         res.json({ message: 'Käyttäjän emailin vaihtaminen onnistui' });
     } catch (error){
         res.json({error: error.message}).status(505);
+    }
+}
+});
+
+router.get('/getPerson', async (req, res) => {
+    const username = req.query.username;
+    if (!username) {
+        return res.status(400).json({ error: 'Käyttäjänimi tarvitaan.' });
+    }else{
+        try {
+            const personData = await getPerson(username);
+            res.json(personData);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
     }
 });
 
