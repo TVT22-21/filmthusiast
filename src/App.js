ab616c10188a8561138095c56a302aed52030fb7
MiikkaJanesaro <122359891+MiikkaJanesaro@users.noreply.github.com>
2023-11-27 15:06:43 +0200

    ylimääräisen poistoa

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import './App.css';
 import { useState } from 'react';
-import { LoginContext } from "./Components/Context";
-import { Login } from './Components/Auth';
 
 function App() {
   const [login, setLogin] = useState(false);
