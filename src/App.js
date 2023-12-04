49950e1caae5b9055304db0b6f4bfc33a6b19c77
MiikkaJanesaro <122359891+MiikkaJanesaro@users.noreply.github.com>
2023-12-04 13:24:47 +0200

    route paths

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import ReactDOM from "react-dom/client"
 import './App.css';
 import { useState } from 'react';
 import RegisterForm from './components/register'
-import Login from './components/login'
+import LoginForm from './components/login'
 import { BrowserRouter, Switch, Route, Routes } from "react-router-dom";
 function App() {
 
 return(
 <div>
-    <RegisterForm/>
+<BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>Home Page</div>} />
+        <Route path="/register" element={<RegisterForm />} />
+        <Route path="/login" element={<LoginForm />} />
+      </Routes>
+    </BrowserRouter>
 </div>
 )
 }
--- src/components/login.js
+++ src/components/login.js
@@ -1,16 +1,24 @@
 import { useState } from "react";
 import axios from "axios";
 import {jwtToken} from './signals';
+import {useNavigate} from "react-router-dom";
 
 export default function LoginForm() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
     //return username.length > 0 && password.length > 0;
 
     function login() {
-      axios.postForm('/postgre/person', {username, password})
-        .then (resp => jwtToken.value = resp.data.jwtToken)
-        .catch(error => console.log(error.message))
+      axios.post('http://localhost:3001/postgre/person', {username, password})
+        .then (resp => {jwtToken.value = resp.data.jwtToken;
+        setError('');
+        navigate('/');
+    })
+        .catch(error => {console.log(error.message);
+        setError('Kirjautuminen epäonnistui. Tarkista salasana.');
+      });
     }
 
 
@@ -20,8 +28,9 @@ export default function LoginForm() {
       <div>
         <h2>Login</h2>
         <input onChange={e => setUsername(e.target.value)}/><br/>
-        <input onChange={e => setPassword(e.target.value)}/><br/>
+        <input type="password" onChange={e => setPassword(e.target.value)}/><br/>
         <button onClick={login}>Kirjaudu sisään</button>
+        {error && {error}}
       </div>
     }
     </div>
