39f98349ce9b61d811b344d33ff12e2a91d00806
MiikkaJanesaro <122359891+MiikkaJanesaro@users.noreply.github.com>
2023-12-05 16:46:39 +0200

    settings page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import ReactDOM from "react-dom/client"
 import './App.css';
 import { useState } from 'react';
-import RegisterForm from './components/register'
+import RegisterForm from './components/register';
 import Profile from './components/profile/profile';
-import Login from './components/login/login'
+import Login from './components/login/login';
+import Settings from './components/settings/settings';
 import { BrowserRouter, Switch, Route, Routes } from "react-router-dom";
 function App() {
 
@@ -15,6 +16,7 @@ return(
         <Route path="/register" element={<RegisterForm />} />
         <Route path="/login" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/settings" element={<Settings />} />
       </Routes>
     </BrowserRouter>
 </div>
