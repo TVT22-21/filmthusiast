8ffa9e6ee0e87f1ec9f577582be45704fef19426
MiikkaJanesaro <122359891+MiikkaJanesaro@users.noreply.github.com>
2023-12-11 21:41:12 +0200

    changemail update

--- src/components/settings/ChangeEmail.js
+++ src/components/settings/ChangeEmail.js
@@ -1,11 +1,20 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+import { userInfo } from '../register/signals';
 
-const ChangeEmail = () => {
-  const [newEmail, setNewEmail] = useState('');
+const ChangeEmail = (username = userInfo.value?.private) => {
+  const [email, setNewEmail] = useState('');
 
   const handleChangeEmail = () => {
-    
+    axios.post("http://localhost:3001/person/updateemail", { username, email })
+    .then((resp)=>{
+    console.log(resp.data);
     console.log('Päivitetään sähköpostia...');
+  })
+    .catch((error) =>{
+      console.log(error.response.data);
+      console.log('Sähköpostin päivitys epäonnistui');
+    });
   };
 
   return (
@@ -13,7 +22,7 @@ const ChangeEmail = () => {
       <h3>Vaihda Sähköposti</h3>
       <label>
         Uusi Sähköposti:
-        <input type="text" value={newEmail} onChange={(e) => setNewEmail(e.target.value)} />
+        <input type="text" value={email} onChange={(e) => setNewEmail(e.target.value)} />
       </label>
       <br />
       <button onClick={handleChangeEmail}>Päivitä Sähköposti</button>
