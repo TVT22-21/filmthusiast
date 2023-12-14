dfd317990eb7e3cdf0b30aeab203b1517f70167f
MiikkaJanesaro <122359891+MiikkaJanesaro@users.noreply.github.com>
2023-12-14 20:21:56 +0200

    testiä

---
+++ test/app.spec.js
@@ -0,0 +1,19 @@
+/*const fetch = require('node-fetch');
+const { expect } = require('chai');
+const  axios = require('axios');
+const chai = require('chai');
+
+const expect = chai.expect;
+
+describe('GET Request Test', () => {
+    it('Should get the user id', await () =>{
+        const user = new User("http://localhost:3001/profile/");
+        const data = await response.json();
+
+        expect(response.status).to.equal(200);
+        expect(data).to.be.an('object');
+        expect(data.entries).to.be.an('array').that.is.not.empty;
+        expect(data.entries[0]).to.include.all.keys('API', 'Description');
+    });
+});
+*/
