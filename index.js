bebb94a1561bbc088af1161201341890347e5370
Anatude1234 <antti.korpua1@gmail.com>
2023-12-14 15:58:14 +0200

    test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const ratingRoute = require('./routes/rating');
 const profileRoute = require('./routes/profileRoute');
 const personRoute = require('./routes/personRoute');
 const loginRoute = require('./routes/login');
-const groupRoute = require('./routes/groupRoute');
+//const groupRoute = require('./routes/groupRoute');
 const app = express();
 const cors = require('cors');
 
@@ -20,10 +20,18 @@ app.use('/login', loginRoute);
 app.use('/profile', profileRoute);
 app.use('/rating', ratingRoute);
 app.use('/person', personRoute);
-app.use('/groups', groupRoute);
+//app.use('/groups', groupRoute);
 
+//app.post('/groups/create', (req, res) => {
+//  console.log('Received request to create a group');
 
-const PORT = process.env.PORT || 3001;
+//});
+//app.post('/groups/join', (req, res) => {
+
+//});
+
+const PORT = process.env.PORT;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
