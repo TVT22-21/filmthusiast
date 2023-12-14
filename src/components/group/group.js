0df28e081c3bf23a104cb5802399ce439f3e6605
Temkhu <teemu.ritatorma@gmail.com>
2023-12-14 20:30:30 +0200

    korjailu

--- src/components/group/group.js
+++ src/components/group/group.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './group.css';
 import {userInfo} from '../register/signals';
-import { Header } from '../header/Header';
+import { Header } from '../header/header';
 
 function Groups() {
   const [groups, setGroups] = useState([]);
